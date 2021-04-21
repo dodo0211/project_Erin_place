652b167b2ba16643c321ec5309bf056b6aa01c29
sensebecommon <79956201+dodo0211@users.noreply.github.com>
2021-04-21 12:29:44 +0900

    footer 구조, 디자인
    구조만 설계, 구체적 내용은 아직 설계 중

---
+++ footer.php
@@ -0,0 +1,37 @@
+<div class="logo"><i class="fas fa-map-marked"></i> <br /> &copy; 2021~</div>
+<div class="features">
+    <h3>Features</h3>
+    <ul class="features-content">
+        <li>Cool stuff</li>
+        <li>Random feature</li>
+        <li>Team feature</li>
+        <li>Stuff for developers</li>
+        <li>Another one</li>
+        <li>Last time</li>
+    </ul>
+</div>
+
+
+<div class="features">
+    <h3>Resources</h3>
+    <ul class="features-content">
+        <li>Cool stuff</li>
+        <li>Random feature</li>
+        <li>Team feature</li>
+        <li>Stuff for developers</li>
+        <li>Another one</li>
+        <li>Last time</li>
+    </ul>
+</div>
+
+<div class="features">
+    <h3>About</h3>
+    <ul class="features-content">
+        <li>Cool stuff</li>
+        <li>Random feature</li>
+        <li>Team feature</li>
+        <li>Stuff for developers</li>
+        <li>Another one</li>
+        <li>Last time</li>
+    </ul>
+</div>
