ae89a927a66d4cc4a393e14aa317b6dcff2af972
sensebecommon <79956201+dodo0211@users.noreply.github.com>
2021-04-21 12:22:16 +0900

    기본 파일 셋팅

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erin's place</title>
+    <!-- link -->
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <!-- script -->
+    <script src="./js/img_slide.js" defer></script>
+    <!-- api -->
+    <script src="https://kit.fontawesome.com/98f04cd3f2.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- header -->
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/project_erin_place/header.php"; ?>
+    </header>
+
+    <!-- section (main) s-->
+    <section>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/project_erin_place/main.php"; ?>
+    </section>
+
+    <!-- footer -->
+    <footer>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/project_erin_place/footer.php"; ?>
+    </footer>
+
+</body>
+
+</html>
