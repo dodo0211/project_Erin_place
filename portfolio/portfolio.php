a486d90e12d95fe2850668d236cfdbc57e8e1b3e
dodo0211 <79956201+dodo0211@users.noreply.github.com>
2021-04-22 00:26:19 +0900

    portfolio 설계, 예시 추가 - 개발 중
     - portfolio 개발 중
     - project_next 예시 1개 추가

---
+++ portfolio/portfolio.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erin's place</title>
+    <!-- link -->
+    <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_erin_place/css/common.css?after=4">
+    <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_erin_place/portfolio/portfolio.css">
+    <!-- script -->
+    <script src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_erin_place/portfolio/portfolio.js" defer></script>
+    <!-- api -->
+    <script src="https://kit.fontawesome.com/98f04cd3f2.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- header -->
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/project_erin_place/header.php"; ?>
+    </header>
+
+    <!-- section (portfolio) -->
+    <section>
+        <h2>Portfolio > the whole</h2>
+        <div>
+            <ul>
+                <li><a href="#">the whole</a></li>
+                <li><a href="#">PHP</a></li>
+                <li><a href="#">Javascrpt</a></li>
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">Java</a></li>
+                <li><a href="#">Python</a></li>
+                <li><a href="#">C</a></li>
+            </ul>
+
+            <div class="portfolio_content">
+                <h3>주식차트 분석 프로그램</h3><br/>
+                <div class="portfolio_icon">                    
+                    <span><a href="https://github.com/dodo0211/ProjectNext" target="_blank"><i class="fab fa-git-square"></i></a></span>
+                    <span><a href="https://www.youtube.com/watch?v=jHTt17o2BgM" target="_blank"><i class="fab fa-youtube-square"></i></a></span>
+                    <span><a href="https://trello.com/b/S9j6XRw2/projectnext" target="_blank"><span>Trello</span></a></span>
+                </div><br/>
+
+                <div>
+                    제작 상세 <br/>
+                    - 개발 기간 : 2021.03.25 ~ <br/>
+                    - 개발 <br/>
+                    팀장 : 송훈일 <br/>
+                    팀원 : 허선영, 김택민 <br/><br/>
+
+                    개발 환경 <br/>
+                    - java 15.0.2 <br/>
+                    - scala 2.13.4 <br/>
+                    - python 3.9.2 <br/>
+                    - android studio 4.1.3, Intellij IDEA 11.0.2, PyCharm 11.0.10 <br/>
+                    - MySQL 8.0.23 <br/><br/>
+
+                    실행 환경 <br/>
+                    - Windows 10 <br/>
+                    - MacOS <br/><br/>
+                </div>
+                
+                <div class="portfolio_img">
+                    <div><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_erin_place/portfolio/img/next/next1.png" alt="프로젝트next_결과사진1"></div>
+                    <div><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_erin_place/portfolio/img/next/next2.png" alt="프로젝트next_결과사진2"></div>
+                    <div><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_erin_place/portfolio/img/next/next3.png" alt="프로젝트next_결과사진3"></div>
+                    <div><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_erin_place/portfolio/img/next/next4.png" alt="프로젝트next_결과사진4"></div>
+                    <div><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_erin_place/portfolio/img/next/next5.png" alt="프로젝트next_결과사진5"></div>
+                </div><br/>                     
+            </div>
+
+        </div>        
+    </section>
+
+    <hr>
+
+    <!-- footer -->
+    <footer>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/project_erin_place/footer.php"; ?>
+    </footer>
+
+</body>
+
+</html>
